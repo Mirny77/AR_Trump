5649d4133e7da13b9f0d3bb16d823a1149a85c17
Mirny77 <mironov.ilya20160@yandex.ru>
2018-08-22 20:49:09 +0300

    first commit

---
+++ Assets/Scripts/TrumpControll.cs
@@ -0,0 +1,37 @@
+﻿using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class TrumpControll : MonoBehaviour {
+	private Animation anim;
+	private Rigidbody tr;
+	private float speed = 4f;
+
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Rigidbody>();
+		anim = GetComponent <Animation>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = CrossPlatformInputManager.GetAxis("Horizontal");
+		float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+		Vector3 mov= new Vector3(x,0,y); 
+
+		tr.velocity = mov * speed;
+
+		if(x !=0 && y!=0){
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x,Mathf.Atan2(x,y) * Mathf.Rad2Deg ,transform.eulerAngles.z);
+		}
+		if(x !=0 && y!=0){
+			anim.Play("woll");
+		}
+		else{
+			anim.Play("Idle");
+		}
+
+		
+	}
+}
